eeed72ff3a0c8eab464219022854e8fb9d4562a6
Sameer <pathaniasameer1997@gmail.com>
2022-08-18 22:53:07 +0530

    first commit

---
+++ GustoRestaurant/src/gustorestaurant/MyConnection.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gustorestaurant;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import java.util.Properties;
+
+/**
+ * @author PC
+ */
+public class MyConnection {
+
+        public static Connection getConnection() {
+        Connection con  = null;
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/testsql", "root", "");
+        } catch (SQLException | ClassNotFoundException |IllegalAccessException|InstantiationException e)
+        {
+            System.out.println("exception occured "+e.getMessage());
+
+        }
+
+        return con;
+    }
+
+    static Object getconnection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
