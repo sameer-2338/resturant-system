87c6a56c0d8ba68376446bbf282362380374360f
Sameer <pathaniasameer1997@gmail.com>
2022-08-19 23:11:10 +0530

    name changed

--- FoodieRestaurant/src/foodierestaurant/FoodieRestaurant.java
+++ FoodieRestaurant/src/foodierestaurant/FoodieRestaurant.java
@@ -15,9 +15,7 @@ import static starting.HomepageForm.offers;
 public class FoodieRestaurant
 {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args)
     {
         // TODO code application logic here
